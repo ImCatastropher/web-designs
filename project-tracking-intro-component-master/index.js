69509d6db1b78000eeed4093ca57a11bf3af2a64
Niteesh Reddy Thota <71356908+ImCatastropher@users.noreply.github.com>
2020-10-11 13:38:39 -0700

    Update index.js

--- project-tracking-intro-component-master/index.js
+++ project-tracking-intro-component-master/index.js
@@ -12,19 +12,19 @@ $("#schedule-button").hover(function(){
   $(this).css("opacity","1");
 });
 
-
-$(".button-1").on("click",()=>{
-
-  $(".button-1").css("visibility","hidden");
-  $(".button-2").css("visibility","visible");
-  $(".background").css("height","600px");
-  $(".navbar-nav").css("box-shadow", "0 0 20px hsl(230, 11%, 40%)");
-  // $(".navbar-nav").css("transition", "height 0.4s ease-in");
-});
-
-$(".button-2").on("click",()=>{
-  $(".button-2").css("visibility","hidden");
-  $(".button-1").css("visibility","visible");
-  $(".background").css("height","450px");
-  $(".navbar-nav").css("box-shadow", "none");
+var count=0;
+$(".navbar-toggler").on("click",()=>{
+  if(count==0){
+    $(".button-1").css("visibility","hidden");
+    $(".button-2").css("visibility","visible");
+    $(".background").css("height","600px");
+    // $(".navbar-nav").css("transition", "height 0.4s ease-in");
+    count+=1;
+  }
+  else{
+      $(".button-2").css("visibility","hidden");
+      $(".button-1").css("visibility","visible");
+      $(".background").css("height","450px");
+      count=0;
+  }
 });
